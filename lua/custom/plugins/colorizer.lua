2986ff6aced00b7f484118930a804cfd8d0f81df
tpad <alex@v.mr>
2026-03-26 07:14:10 +0000

    colorizer

---
+++ lua/custom/plugins/colorizer.lua
@@ -0,0 +1,15 @@
+return {
+	"catgoose/nvim-colorizer.lua",
+	event = "BufReadPre",
+	opts = {
+		options = {
+			parsers = {
+				css = true
+			},
+			display = {
+				mode = "virtualtext",
+				virtualtext = { position = "after" },
+			},
+		}
+	}
+}
